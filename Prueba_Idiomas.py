a04301cfabe376360db8ea62b8f69aeb6fca46da
juanaves <juanaves@gmail.com>
2020-04-25 10:27:08 +0200

    modificado fichero json

--- Prueba_Idiomas.py
+++ Prueba_Idiomas.py
@@ -2,12 +2,43 @@ import json
 from pathlib import Path
 import requests
 import os
+import sys
+
+
 try:
+    def numero_pruebas(datos):
+        print("Numero de pruebas : %s"%len(datos))
+    
+    def pruebas_mas_2_horas(datos):
+        for prueba in datos:
+            if prueba["Horas"]>2:
+                print(prueba['Titulo'])
+
+    def url_pruebas_no_presencial(datos):
+        for prueba in datos:
+            if prueba["TipoFormacion"]=="NoPresencial":
+                print(prueba['URL'])
+
+    def titulo_profesores_id_prueba(datos):
+        id_prueba=int(input("Introduce el id de la prueba: "))
+        if datos["$id"]==id_prueba:
+            print("datos['Titulo']")
+            for profesor in datos["Profesorado"]:
+               print(profesor["NombreCompleto"])
+            
+
+
+
+
+
 
 #estas 3 lineas es para abrir un fichero xml, son siempre las mismas y me devuelve en la ¿variable(objeto)? arbol todo el xml
     
     base_path = Path(__file__).parent
     file_path = (base_path / "prueba_idiomas.json").resolve()
+    with open(file_path,'r') as file:
+        #json.load. devuelve un objeto de tipo diccionario sobre el que se puede iterar.
+        datos = json.load(file)
     
 
     while True:
@@ -25,17 +56,19 @@ try:
             except ValueError:
                 print('Error, introduce un numero entero')
         if opcion == 1:
-            listado_provincias(arbol)
+            numero_pruebas(datos)
         elif opcion == 2:
-            listado_provincias_municipios(arbol)
+            pruebas_mas_2_horas(datos)
         elif opcion == 3:
-            municipios_provincia(arbol)
+            url_pruebas_no_presencial(datos)
         elif opcion == 4:
-            provincia_de_un_municipio(arbol)
-        elif opcion == 5:
+           titulo_profesores_id_prueba(datos)
+        elif opcion == 6:
             break
         else:
             print ("Introduce un numero entre 1 y 3")
+        
+        input ("Pulsa una tecla para continuar ....")
 
   
 
